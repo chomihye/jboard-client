788c6503cc257f49be4255a494c71eb7027f238e
chomihye <devmihye@gmail.com>
2023-01-01 23:46:57 +0900

    게시판 목록조회 화면 및 상세 화면 생성

---
+++ data/notices.js
@@ -0,0 +1,34 @@
+const notices_data = {
+  notices: [
+    {
+      idx: 1,
+      title: '굿노트 활용법',
+      writer: 'mimi',
+      created_at: '2022-01-05',
+    },
+    {
+      idx: 2,
+      title: '다이어리용 펜 추천',
+      writer: 'bani',
+      created_at: '2022-02-02',
+    },
+    {
+      idx: 3,
+      title: '노션 템플릿 공유합니다',
+      writer: 'bori',
+      created_at: '2022-03-16',
+    },
+    {
+      idx: 4,
+      title: '아이패드 케이스 추천',
+      writer: 'kado',
+      created_at: '2022-04-13',
+    },
+    {
+      idx: 5,
+      title: '버터크림 다이어리 후기',
+      writer: 'cream',
+      created_at: '2022-05-05',
+    },
+  ],
+};
