0931e0b99ab503744a021ef2120a9fcccba60385
chomihye <devmihye@gmail.com>
2023-01-03 23:14:36 +0900

    게시글 내용 수정

--- data/notices.js
+++ data/notices.js
@@ -5,30 +5,42 @@ const notices_data = {
       title: '굿노트 활용법',
       writer: 'mimi',
       created_at: '2022-01-05',
+      contents: '굿노트를 활용하는 방법에 대해 알아봅시다',
+      img: 'https://static.wixstatic.com/media/ca54a1_46dbe13f4e6e4794971e9ec79fd92af1~mv2.jpg/v1/fill/w_740,h_740,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/ca54a1_46dbe13f4e6e4794971e9ec79fd92af1~mv2.jpg',
     },
     {
       idx: 2,
       title: '다이어리용 펜 추천',
       writer: 'bani',
       created_at: '2022-02-02',
+      contents:'제트스트림 펜이 괜찮은것같아요. 형광펜은 마일드라이너 추천합니다',
+      img: 'https://static.wixstatic.com/media/ca54a1_46dbe13f4e6e4794971e9ec79fd92af1~mv2.jpg/v1/fill/w_740,h_740,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/ca54a1_46dbe13f4e6e4794971e9ec79fd92af1~mv2.jpg',
     },
     {
       idx: 3,
       title: '노션 템플릿 공유합니다',
       writer: 'bori',
       created_at: '2022-03-16',
+      contents:
+        '얼마전에 만든 노션 템플릿 공유합니다. 회의록 정리하기 편합니다',
+      img: 'https://static.wixstatic.com/media/ca54a1_46dbe13f4e6e4794971e9ec79fd92af1~mv2.jpg/v1/fill/w_740,h_740,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/ca54a1_46dbe13f4e6e4794971e9ec79fd92af1~mv2.jpg',
     },
     {
       idx: 4,
       title: '아이패드 케이스 추천',
       writer: 'kado',
       created_at: '2022-04-13',
+      contents: '어프어프 아이패드 케이스가 귀여워요. 커버가 달려 있어요',
+      img: 'https://static.wixstatic.com/media/ca54a1_46dbe13f4e6e4794971e9ec79fd92af1~mv2.jpg/v1/fill/w_740,h_740,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/ca54a1_46dbe13f4e6e4794971e9ec79fd92af1~mv2.jpg',
     },
     {
       idx: 5,
       title: '버터크림 다이어리 후기',
       writer: 'cream',
       created_at: '2022-05-05',
+      contents:
+        '일요일 시작인줄 알고 샀는데 월요일 시작이라 달력 볼때 조금 헷깔리네요',
+      img: 'https://static.wixstatic.com/media/ca54a1_46dbe13f4e6e4794971e9ec79fd92af1~mv2.jpg/v1/fill/w_740,h_740,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/ca54a1_46dbe13f4e6e4794971e9ec79fd92af1~mv2.jpg',
     },
   ],
 };
